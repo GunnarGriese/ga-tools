97151c426fd34ec152b80115f838d4ed804b05e3
IIH Nordic <gunnargriese@gmx.de>
2020-04-28 16:33:03 +0200

    filter to sheets

--- filter_download.py
+++ filter_download.py
@@ -12,10 +12,20 @@ parser.add_argument('--view-id', help='GA View ID')
 
 args = parser.parse_args()
 
-SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
-API_NAME = 'analytics'
-API_VERSION = 'v3'
-CLIENT_SECRET_FILE = 'credentials/tools@iih_aller-leisure.json'
+SCOPES = [
+    'https://www.googleapis.com/auth/analytics.readonly', 
+    'https://www.googleapis.com/auth/spreadsheets'
+]
+
+MGMT_API = 'analytics'
+MGMT_VERSION = 'v3'
+SHEETS_API = 'sheets'
+SHEETS_VERSION = 'v4'
+SHEET_ID = '1HcE6Sp2gDScz2drm7foVaSPTmF_69Y_p7KK6ZCnAzhc'
+RANGE_NAME = 'A1:AA1000'
+
+
+CLIENT_SECRET_FILE = 'credentials/tools@iih.json'
 
 if args.account_id: # when run from cmd line
     ACCOUNT_ID = args.account_id
@@ -26,9 +36,8 @@ else: # change when run directly from editor
     PROPERTY_ID = 'UA-6219026-4'
     VIEW_ID = '15147521'
 
-
-def get_account_filters(api_service, account_id):
-    response = api_service.management().filters().list(accountId=account_id).execute()
+def get_account_filters(mgmt_api, account_id):
+    response = mgmt_api.management().filters().list(accountId=account_id).execute()
     filters = response.get('items', [])
     return filters
 
@@ -54,31 +63,49 @@ def handle_filters(filter_list):
 
     return filter_dict
 
-def get_view_filters(api_service, account_id, property_id, view_id):
-    response = api_service.management().profileFilterLinks().list(
+def get_view_filters(mgmt_api, account_id, property_id, view_id):
+    response = mgmt_api.management().profileFilterLinks().list(
       accountId=account_id,
       webPropertyId=property_id,
       profileId=view_id).execute()
     view_filters = response.get('items', [])
     return view_filters
 
+def upload_to_sheets(sheets_service, sheet_id, range_name, df):
+    sheets_service.spreadsheets().values().update(
+        spreadsheetId=sheet_id,
+        valueInputOption='RAW',
+        range=range_name,
+        body=dict(
+            majorDimension='ROWS',
+            values=df.T.reset_index().T.values.tolist())
+    ).execute()
+
 if __name__ == '__main__':
     """Print all filter (details) associated with a view to console"""
-    api_service = google_auth.authenticate_to_google(CLIENT_SECRET_FILE, SCOPES, API_NAME, API_VERSION)
-    filter_list = get_account_filters(api_service, ACCOUNT_ID)
+    mgmt_api = google_auth.authenticate_to_google(CLIENT_SECRET_FILE, SCOPES, MGMT_API, MGMT_VERSION)
+    sheets_api = google_auth.authenticate_to_google(CLIENT_SECRET_FILE, SCOPES, SHEETS_API, SHEETS_VERSION)
+    filter_list = get_account_filters(mgmt_api, ACCOUNT_ID)
     filter_dict = handle_filters(filter_list)
-    view_filters = get_view_filters(api_service, ACCOUNT_ID, PROPERTY_ID, VIEW_ID)
-    filter_ranks = []
+    view_filters = get_view_filters(mgmt_api, ACCOUNT_ID, PROPERTY_ID, VIEW_ID)
+
     filter_names = []
     for i in view_filters:
-        filter_ranks.append(i['rank'])
         filter_names.append(i['filterRef']['name'])
+    
+    filter_ids = []
+    filter_name = []
+    filter_types = []
+    filter_details = []
 
     for key, value in filter_dict.items():
         if value.name in filter_names:
-            print("Filter: {}".format(value.id))
-            print("Name: {}".format(value.name))
-            print("Type: {}".format(value.type))
-            #print("LAst Update: {}".format(value.updated))
-            print(json.dumps(value.details, indent=2))
-            print("--------------")
+            filter_ids.append(value.id)
+            filter_name.append(value.name)
+            filter_types.append(value.type)
+            filter_details.append(json.dumps(value.details))
+    df = pd.DataFrame(
+        list(zip(filter_ids, filter_names, filter_types, filter_details)),
+        columns = ['ID', 'Name', 'Type', 'Details']
+    )
+    upload_to_sheets(sheets_api, SHEET_ID, RANGE_NAME, df)
